36e80baca02d2777124bb0acf3eb6c3e21c16bcd
rudra-iitm <rudransh.iitm@gmail.com>
2025-01-24 23:48:39 +0530

    Added splash loading logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,45 @@
+'use client';
 import AboutMe from "@/components/aboutme";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Works from "@/components/works";
+import { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import Splash from "@/components/ui/splash";
 
 export default function Home() {
+  const [showSplash, setShowSplash] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <div className="flex justify-center min-h-screen items-center">
+      <AnimatePresence mode="wait">
+        {showSplash ? (
+          <Splash key="splash" />
+        ) :
+        <motion.div
+        key="main-content"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="min-h-screen flex flex-col items-center justify-center"
+      >
       <div className="h-full">
         <Intro />
         <AboutMe />
         <Experience />
         <Works />
       </div>
+      </motion.div>
+        }
+    </AnimatePresence>
     </div>
   );
 }
