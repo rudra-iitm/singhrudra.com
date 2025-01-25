c689119980ab1f3411e14e0373c493d5706ac046
rudra-iitm <rudransh.iitm@gmail.com>
2025-01-26 00:25:26 +0530

    Add subject and body to mailto

--- components/email-sidebar.tsx
+++ components/email-sidebar.tsx
@@ -23,7 +23,7 @@ export default function EmailSidebar() {
     >
       <div className="flex flex-col items-center gap-6">
         <Link
-          href="mailto:rudransh.iitm@gmail.com"
+          href={`mailto:rudransh.iitm@gmail.com?subject=Inquiry%20from%20Your%20Portfolio&body=Hi%20there%2C%0D%0A%0D%0AI%20hope%20this%20message%20finds%20you%20well.%20I%20came%20across%20your%20portfolio%20and%20wanted%20to%20reach%20out%20regarding%20[Your%20specific%20topic%20or%20project].%0D%0A%0D%0APlease%20let%20me%20know%20a%20convenient%20time%20for%20us%20to%20discuss%20this%20further.%0D%0A%0D%0ABest%20regards%2C%0D%0A[Your%20Name]%0D%0A[Your%20Contact%20Information]`}
           className="vertical-text hover:text-violet-600 transition-all duration-300 py-2 px-1"
         >
           rudransh.iitm@gmail.com
