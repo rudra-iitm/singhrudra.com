7b2e4a50cfe09dbe848da627fe6e2aab3030aa3b
Rudra Pratap Singh <rudransh.iitm@gmail.com>
2026-03-05 23:47:13 +0530

    Fix basePath configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/singhrudra.com" : "",
   images: {
     unoptimized: true, // Disable Image Optimization for static exports
   },
